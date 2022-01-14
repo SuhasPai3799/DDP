b35e58c6f06258be6d613fd55241a74120e6824c
SuhasPai3799 <cs17b116@smail.iitm.ac.in>
2022-01-14 10:00:05 +0530

    Course details functional with course ID

--- examples/chatcat/src/main/java/de/dfki/chatcat/HFCUtils.java
+++ examples/chatcat/src/main/java/de/dfki/chatcat/HFCUtils.java
@@ -47,15 +47,20 @@ public class HFCUtils{
 		return String.valueOf(res.size());
 		
 	}
+	/**
+	 * Given a department name, return the name of a close department name as stored in the database.
+	 * @param dept_name Name of the department for which we are trying to find the closest possible name in the database.
+	 * @return "NULL" if no department is found close enough.
+	 */
 	public static String getClosestDept(String dept_name)
 	{
-		logger.log(Level.INFO, "Hello");
+		
 		for (Map.Entry mapElement : dept_syns.entrySet()) {
 			String dept_official_name = (String)mapElement.getKey();
 			List<String> dept_name_syns = (List)mapElement.getValue();
 			for(String syn: dept_name_syns)
 			{
-				if(syn.equals(dept_name))
+				if((syn.toLowerCase()).equals(dept_name.toLowerCase()))
 				{
 					return dept_official_name;
 				}
@@ -65,6 +70,46 @@ public class HFCUtils{
 		}
 		return "NULL";
 	}
+	/**
+	 * Given the name of a course, return the details of that particular course
+	 * @param c_name
+	 * @return
+	 */
+	public static String answerCourseInfo(String c_name)
+	{
+		String c_name_uri = "<univ:" + c_name + ">";
+		if(c_name.matches("[a-zA-Z][a-zA-Z][0-9]+"))
+		{
+			String check_course = c_name.toLowerCase();
+			String query = "select ?a where ?a <rdf:type> <univ:Courses> ?d";
+			List<Object> res = _agent._proxy.query(query);
+			
+
+			for(Object course: res)
+			{
+				String course_id = String.valueOf(course);
+				
+				if(c_name_uri.toLowerCase().equals(course_id.toLowerCase()))
+				{	
+					logger.log(Level.INFO, "Working");
+					String course_comment_query = String.format("select ?a where %s <rdfs:comment> ?a ?x", course_id);
+					List<Object> comments = _agent._proxy.query(course_comment_query);
+					return (String)(comments.get(0));
+				}
+			}
+		}
+		return "Course Info";
+	}
+
+	// public static List<String> answerDeptCoursesOffered(String dept_name)
+	// {
+	// 	String closest_dept_name = getClosestDept(dept_name)
+	// 	if(closest_dept_name == "NULL")
+	// 	{
+	// 		return "-1";
+	// 	}
+	// 	String dept_uri 
+	// }
 	public HFCUtils(ChatAgent agent)
 	{
 		this._agent = agent;
