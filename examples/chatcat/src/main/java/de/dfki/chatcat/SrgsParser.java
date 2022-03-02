9440f1dc148a9ae5d8c9789cc7973e7e018156b6
SuhasPai3799 <cs17b116@smail.iitm.ac.in>
2022-03-02 18:25:55 +0530

    Configured RasaNLU successfully

--- examples/chatcat/src/main/java/de/dfki/chatcat/SrgsParser.java
+++ examples/chatcat/src/main/java/de/dfki/chatcat/SrgsParser.java
@@ -4,6 +4,7 @@ import static de.dfki.chatcat.Constants.*;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
 import java.util.Map;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -18,6 +19,19 @@ import org.jvoicexml.processor.srgs.grammar.Grammar;
 import de.dfki.mlt.rudimant.agent.DialogueAct;
 import de.dfki.mlt.rudimant.agent.nlg.Interpreter;
 import de.dfki.mlt.srgsparser.JSInterpreter;
+
+import java.util.Scanner;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.NameValuePair;
+import java.util.*;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.entity.StringEntity;
+
+import org.json.JSONObject;
 
 public class SrgsParser extends Interpreter {
   JVoiceXmlGrammarManager manager;
@@ -52,11 +66,65 @@ public class SrgsParser extends Interpreter {
     text = text.replace(","," ");
     return text;
   }
+
+  public void rasaNLU(String text)
+  {
+      CloseableHttpClient httpclient = HttpClients.createDefault();
+
+      //Creating a HttpGet object
+      JSONObject json = new JSONObject();
+      StringEntity params = null;
+      json.put("text", text);
+      try{
+      params = new StringEntity(json.toString());
+      }
+      catch(UnsupportedEncodingException e)
+      {
+        e.printStackTrace();
+      }
+      
+      HttpPost httppost = new HttpPost("http://localhost:5005/model/parse");
+      
+      httppost.setHeader("Content-type", "application/json");
+      httppost.setEntity(params);
+      
+    
+      //Printing the method used
+     
+      HttpResponse httpresponse = null;
+      Scanner sc = null;
+      //Executing the Get request
+      try{
+      httpresponse = httpclient.execute(httppost);
+      }
+      catch(IOException e)
+      {
+        e.printStackTrace();
+      }
+
+      try
+      {
+        sc = new Scanner(httpresponse.getEntity().getContent());
+      }
+      catch(IOException e)
+      {
+        e.printStackTrace();
+      }
+
+      //Printing the status line
+      System.out.println(httpresponse.getStatusLine());
+      while(sc.hasNext()) {
+         System.out.println(sc.nextLine());
+      }
+  }
+
+
   @Override
   public DialogueAct analyse(String text) {
     
     text = clean(text);
     System.out.println(text);
+    rasaNLU("check");
     String[] tokens = text.split(" +");
     try {
       //TODO: Find out why no validRule is returned
